0ae6a046c14ed868b0d0bf94ce3c5dea7b44eac0
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-05-25 00:38:03 -0400

    add delete marked activities and all of them of todolist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,5 +4,15 @@ class HomeController < ApplicationController
 
   def about
   end 
+
+  def trashall 
+    @trashall = List.where(:completed => true).destroy_all
+    redirect_to lists_url, notice: "All mark items were successfully deleted " 
+  end 
+
+  def trashthemall
+    @trashthemall = List.all.destroy_all
+    redirect_to lists_url, notice: "All mark items were successfully deleted " 
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :contacts
   get 'home/about'
+  get 'home/trashall'
+  get 'home/trashthemall'
   root 'home#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
