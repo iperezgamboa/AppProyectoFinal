95de451cc6878e4a419debba5887c960264aa65e
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-05-27 03:20:10 -0400

    price and blog data api cryptocurrency

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,5 +14,22 @@ class HomeController < ApplicationController
     @trashthemall = List.all.destroy_all
     redirect_to lists_url, notice: "All mark items were successfully deleted " 
   end
-  
+
+  def prices
+    require 'net/http'
+    require 'json'
+    
+    #News info data
+    @url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @news = JSON.parse(@response)
+    
+    #Grab price data
+    @prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XLM,MIOTA,USDT,TRX&tsyms=USD,EUR'
+    @prices_uri = URI(@prices_url)
+    @prices_response = Net::HTTP.get(@prices_uri)
+    @prices = JSON.parse(@prices_response)
+    
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'home/about'
   get 'home/trashall'
   get 'home/trashthemall'
+  get 'home/prices'
   root 'home#index'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
